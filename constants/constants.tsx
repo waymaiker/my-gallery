5be371957a578a1c14744a7ebeb6b28c21d7439b
Bonkomprann <waymaiker@laposte.net>
2023-07-16 22:47:01 +0200

    Change icon to add photo

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { AntDesign } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 
 export const Icons:any = {
   "camera": <Ionicons name="md-camera" size={32} />,
   "trash": <Ionicons name="ios-trash-bin" size={32} />,
   "cross": <Entypo name="cross" size={24} color="black" />,
+  "add": <AntDesign name="plus" size={24} color="black" />
 }
