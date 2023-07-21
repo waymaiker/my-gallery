e5cf64b7fb3786363e12664876d9e24d5f46b6ac
Bonkomprann <waymaiker@laposte.net>
2023-07-21 16:58:37 +0200

    Quick wins

--- screens/CardScreen.tsx
+++ screens/CardScreen.tsx
@@ -3,6 +3,7 @@ import { Text, View } from "react-native";
 
 
 import ImageCard from "../components/ImageCard";
+import usePhoneOrientationProvider from "../hooks/usePhoneOrientationProvider";
 
 type CardScreenProps = {
   route: { params: { title: string, uri: string } }
@@ -10,16 +11,19 @@ type CardScreenProps = {
 
 export default function CardScreen({route}: CardScreenProps) {
   const { params: { title, uri } } = route;
+  const { isPortrait } = usePhoneOrientationProvider()
 
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: "center" }}>
+    <View style={{ flexDirection: isPortrait ? 'column' : 'row', alignItems: 'center', justifyContent: "center", padding: 20 }}>
       <Text style={{
         backgroundColor: "white",
         textAlign: 'center',
         fontSize: 20,
         borderRadius: 10,
+        padding: 10,
+        marginBottom: 10
       }}>
-        Name: {title}
+        {title}
       </Text>
       <ImageCard
         uri={uri}
