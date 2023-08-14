ef931b22f1db5cef39abc743df999a8437e99f24
Bonkomprann <waymaiker@laposte.net>
2023-08-14 17:55:55 +0200

    Add new icon

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -8,6 +8,7 @@ export const Icons:any = {
   "trash": <Ionicons name="ios-trash-bin" size={32} />,
   "cross": <Entypo name="cross" size={24} color="black" />,
   "add": <AntDesign name="plus" size={24} color="black" />,
+  "add-white": <AntDesign name="plus" size={44} color="white"/>,
   "grid": <Fontisto name="nav-icon-grid" size={24} color="black" />,
   "square": <Ionicons name="ios-square" size={24} color="black" />
 }
